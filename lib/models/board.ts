9a48f82219858f31db90d8179eab39e61f67c432
Ezeoye Goodluck <116758130+EzeoyeGoodluck@users.noreply.github.com>
2026-07-14 23:29:30 +1200

    Job application dashboard  Models and schema

--- lib/models/board.ts
+++ lib/models/board.ts
@@ -1,4 +1,37 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
+export interface IBoard extends Document {
+  name: string;
+  userId: string;
+  columns: mongoose.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
 
-export interface IBoard
+const BoardSchema = new Schema<IBoard>(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    columns: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Column",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Board ||
+  mongoose.model<IBoard>("Board", BoardSchema);
